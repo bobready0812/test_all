f5348a6be44ce006bbe44296e418973514c17d64
DaehyeonKim <Daehyeonoff@gmail.com>
2022-06-08 12:22:13 +0000

    20220608

--- login_test/src/App.js
+++ login_test/src/App.js
@@ -53,7 +53,7 @@ const App = () => {
       <input
         value={inputValue2}
         onChange={changeValue2}
-        placeholder="비번"
+        placeholder="비번이요"
       ></input>
       <input
         value={inputValue3}
